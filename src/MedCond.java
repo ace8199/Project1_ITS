f6d4938e54c8098afdd31e5b32849b4f97d9080c
Tony Zheng <tony.zheng@uconn.edu>
2020-10-13 22:09:31 -0400

    MedCond and TravProfDB

--- src/MedCond.java
+++ src/MedCond.java
@@ -1,2 +1,37 @@
 public class MedCond {
+    String mdContact, mdPhone, algType, illType;
+
+    public MedCond(String mdContact, String mdPhone, String algType, str illType) {
+        this.mdContact = mdContact;
+        this.mdPhone = mdPhone;
+        this.algType = algType;
+        this.illType = illType;
+    }
+
+    public String getMdContact() {
+        return this.mdContact;
+    }
+    public String getMdPhone() {
+        return this.mdPhone;
+    }
+    public String getAlgType() {
+        return this.algType;
+    }
+    public String getIllType() {
+        return  this.illType;
+    }
+
+    public void updateMdContact(String mdContact) {
+        this.mdContact = mdContact;
+    }
+    public void updateMdPhone(String mdPhone) {
+        this.mdPhone = mdPhone;
+    }
+    public void updateAlgType(String algType) {
+        this.algType = algType;
+    }
+    public void updateIllType(String illType) {
+        this.illType = illType;
+    }
+
 }
