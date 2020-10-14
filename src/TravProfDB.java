46dacc35a523baef950672edd9c7c049d7c22fa2
Tony Zheng <tony.zheng@uconn.edu>
2020-10-13 22:09:11 -0400

    MedCond and TravProfDB

--- src/TravProfDB.java
+++ src/TravProfDB.java
@@ -1,2 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TravProfDB {
+    int numTravelers, currentTravelerIndex;
+    String fileName;
+    TravProf[] travelerList;
+
+    public TravProfDB(String fileName) {
+        initializeDataBase(fileName);
+        findFirstProfile()
+    }
+
+    public insertNewProfile(TravProf profile) {
+
+    }
+
+    private initializeDataBase(String fileName) {
+        Scanner s = new Scanner(new File(fileName));
+        while (s.hasNextLine()){
+            insertNewProfile(s.nextLine());
+        }
+    }
 }
